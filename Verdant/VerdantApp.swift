32f9fa6878e4bd94e46ce5b4c85b261fa3c95a27
Ryan Tessier <tessier.ryan.m@gmail.com>
2026-03-07 17:08:39 +1100

    Initial Commit

---
+++ Verdant/VerdantApp.swift
@@ -0,0 +1,17 @@
+//
+//  VerdantApp.swift
+//  Verdant
+//
+//  Created by Ryan Tessier on 7/3/2026.
+//
+
+import SwiftUI
+
+@main
+struct VerdantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
